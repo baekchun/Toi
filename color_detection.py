c9368c1fdb36e529b8152e057449724505eed54f
Baekchun Kim <baekchunkim@MacBook-Pro-2.local>
2018-02-19 17:26:05 -0500

    made style changes

--- color_detection.py
+++ color_detection.py
@@ -7,10 +7,12 @@ image = cv2.imread("images/blood_in_stool_sample.jpg")
 image = cv2.resize(image, (500, 375))
 
 # set range of color RED
+# Note: numpy takes the values in the order of BGR, not RGB
 lower = np.array([17, 15, 100], dtype = "uint8")
 upper = np.array([50, 56, 200], dtype = "uint8")
-mask = cv2.inRange(image, lower, upper)
 
+# mask the non-red parts of an image
+mask = cv2.inRange(image, lower, upper)
 output = cv2.bitwise_and(image, image, mask=mask)
 
 # show the image
